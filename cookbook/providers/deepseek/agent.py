c217c193883232c3b0612ef0a36cdb08d7639b5e
Shivam singh <112329366+Shivam909058@users.noreply.github.com>
2024-12-20 13:58:57 +0530

    Update agent.py

--- cookbook/providers/deepseek/agent.py
+++ cookbook/providers/deepseek/agent.py
@@ -1,5 +1,16 @@
 """Run `pip install yfinance` to install dependencies."""
-
+# IMPORTANT: Before running this script, ensure that the (ANY MODEL )API_KEY environment variable is set.
+# To set the API key, follow the instructions below:
+# 
+# On Windows:
+# Open Command Prompt as Administrator and run:
+# setx DEEPSEEK_API_KEY "your-api-key-here"
+#
+# On Unix/Linux:
+# Open a terminal and run:
+# export DEEPSEEK_API_KEY="your-api-key-here"
+#
+# NOTE: After setting the API key, restart your terminal or environment to apply the changes  by shuting down the terminal and run the script again.
 from phi.agent import Agent, RunResponse  # noqa
 from phi.model.deepseek import DeepSeekChat
 from phi.tools.yfinance import YFinanceTools
